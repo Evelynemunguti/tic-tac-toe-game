985ddebc90df8a41e392cbb1222a4fabdc47d21f
Evelyne Munguti <evelynekyamwai@gmail.com>
2025-05-15 23:57:36 +0300

    Added document.queryselectorall to select all cell classes that make up the grids

---
+++ script.js
@@ -0,0 +1,5 @@
+const gameBoard = document.getElementById("gameBoard");
+const status = document.getElementById("status");
+const resetBtn = document.getElementById("resetBtn");
+
+const cell = document.querySelectorAll(".cell");
