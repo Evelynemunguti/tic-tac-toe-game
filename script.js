d4a2f96e2971345db50c499fb11748a9d7946912
Evelyne Munguti <evelynekyamwai@gmail.com>
2025-05-22 05:45:39 +0300

    Added function to check the winner and eventlistener to make the game valuable

--- script.js
+++ script.js
@@ -1,5 +1,48 @@
-const gameBoard = document.getElementById("gameBoard");
+const gameBoard = document.getElementById("gameBoord");
 const status = document.getElementById("status");
 const resetBtn = document.getElementById("resetBtn");
 
+
+
+let currentPlayer = "X";
+
 const cell = document.querySelectorAll(".cell");
+
+cell.forEach(cell=>{
+    cell.addEventListener("click",()=>{ //will only apply when the cell is empty
+if(cell.textContent === ""){
+cell.textContent =currentPlayer;
+currentPlayer= currentPlayer==="X"?"O": "X";
+};
+    });
+});
+
+
+const winningCombos=[
+
+  [0, 1, 2], // Top row
+  [3, 4, 5], // Middle row
+  [6, 7, 8], // Bottom row
+  [0, 3, 6], // Left column
+  [1, 4, 7], // Middle column
+  [2, 5, 8], // Right column
+  [0, 4, 8], // Diagonal top-left to bottom-right
+  [2, 4, 6]  // Diagonal top-right to bottom-left
+];
+
+function checkWinner(){
+
+    for (let combo of winningCombos){
+
+        const[ a,b,c]= combo;
+
+        if (
+            cells[a].textContent !== "" &&
+            cells[a].textContent === cells[b].textContent &&
+            cells[a].textContent === cells[c].textContent
+          ) {
+            return cells[a].textContent; // returns "X" or "O"
+          }
+        }
+        return null; // No winner yet
+    }
